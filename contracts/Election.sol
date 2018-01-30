57067d51a16a4aa6daa0d8604fcbc574237946cc
Ash Smith <ash@getsaven.com>
2018-01-30 13:47:46 +0800

    Initial compile

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+pragma solidity ^ 0.4.11;
+
+contract Election {
+
+	//Read Candidate
+	string public candidate;
+
+	//constructor
+	function Election () public {
+		candidate = "Candidate 1";
+	}
+
+
+}
